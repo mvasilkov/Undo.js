43ecb22c9e4f38de63b44db70f5987207cd6f0f1
Mark Vasilkov <mvasilkov@gmail.com>
2013-03-23 17:14:31 +0200

    Basic usage

---
+++ test.js
@@ -0,0 +1,32 @@
+var should = require("should"),
+    Undo = require("./undo")
+
+describe("Undo", function () {
+    "use strict"
+
+    var undo = new Undo
+
+    it("should start with an empty stack", function () {
+        undo.stack.length.should.equal(0)
+    })
+
+    describe("#rec()", function () {
+        var obj = {}
+
+        it("should save patches on stack", function () {
+            undo.rec(obj, function () {
+                obj.a = "obj.a"
+            })
+
+            undo.stack.length.should.equal(1)
+        })
+
+        it("should not save null patches", function () {
+            undo.rec(obj, function () {
+                // do nothing
+            })
+
+            undo.stack.length.should.equal(1)
+        })
+    })
+})
--- undo.js
+++ undo.js
@@ -5,9 +5,36 @@ if (typeof define != "function" && typeof module == "object") {
 }
 
 define(["./lib/diff-match-patch"], function (lib) {
-    function Undo(init) {
-        this.init = init || null
+    "use strict"
+
+    var utils = new lib.diff_match_patch
+
+    function makePatch(a, b) {
+        var diff = utils.diff_main(b, a)
+
+        utils.diff_cleanupEfficiency(diff)
+
+        return utils.patch_make(b, diff)
+    }
+
+    function Undo() {
         this.stack = Array()
+        this.p = 0
+    }
+
+    Undo.prototype.rec = function (obj, fun) {
+        var a = JSON.stringify(obj),
+            b = JSON.stringify(fun(obj) || obj),
+            patch = makePatch(a, b)
+
+        if (this.stack.length > this.p) {
+            this.stack.length = this.p
+        }
+
+        console.assert(this.p === this.stack.length)
+
+        this.stack.push(patch)
+        ++this.p
     }
 
     return Undo
